9056d80d6e281233d68717f6e8160a44832568a2
zaalTsagareli <zaali.tsagareli@makingscience.com>
2023-05-24 00:19:18 +0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,39 @@
-import * as http from "http";
+import { RCPServer } from "./rcp-server/rcp-server";
+import { Transport } from "./interfaces/transport-interface";
+import net from "net";
+import http from "http";
 
-import { Server } from "./server/rpc.server";
+class TcpServer implements Transport {
+  port: number;
+  protocol: string = "tcp";
+  constructor(port: number) {
+    this.port = port;
+  }
 
-const rcpServer = new Server();
-rcpServer.listen(3000);
+  start(): void {
+    const socket = net.createServer();
+    socket.listen(this.port, "localhost", () => {
+      console.log("tcpserver is running");
+    });
+  }
+}
+
+class HttpServer implements Transport {
+  port: number;
+  protocol: string = "http";
+  constructor(port: number) {
+    this.port = port;
+  }
+  start(): void {
+    const httpServer = http.createServer();
+    httpServer.listen(this.port, "localhost", () => {
+      console.log("httpserver started");
+    });
+  }
+}
+const transports = [new TcpServer(3000), new HttpServer(3001)];
+const rcpServer = new RCPServer(transports);
+ო9ლკ;
+``;
+
+rcpServer.run();
